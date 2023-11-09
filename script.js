e4c58d076870d2c89c05e864611bea3ebaaea3ce
Elinardo <costa.elinardo@gmail.com>
2023-11-08 22:43:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,86 +28,87 @@ function searchCodes() {
     const resultsDiv = document.getElementById('resultsDiv');
 
     const selectedFile = fileInput.files[0];
-    const codesList = [
-        // Lista de RF a serem pesquisados        
-        "9233521",  // ADRIANO ANTUNES DE SALVES
-        "8285187",  // ALESSANDRA RAYMUNDO
-        "7971427",  //ALESSANDRA SERICAWA BROCCO SILVA
-        "8398542",  //AMANDA ALVES DOS SANTOS PEDRO
-        "8029482",  //ANA LUCIA CARVALHO
-        "8791210",  //ANA PAULA RODRIGUES HONORIO
-        "8063494",  //ANA PAULA VIANA COSTA
-        "8821186",  //ANGELA MARIA DE OLIVEIRA
-        "8259640",  //APARECIDA VIEIRA DA SILVA
-        "8171831",  //BEATRIZ CAROLINE BISPO DIAS
-        "8421790",  //BEATRIZ NOGUEIRA DE SOUSA
-        "7822529",  //CARLA FERNANDA BARRETO
-        "8495955",  //CAROLINA LIMA ANTUNES
-        "8279071",  //CELIA REGINA LUNA TOPGIAN
-        "8163481",  //CLAUDIA MARIA AMADOR RUSSO
-        "8259011",  //CLAUDINEI DA SILVA CRUZ
-        "7736835",  //CLAUDIO VIEIRA DA SILVA
-        "8139539",  //CRISTINA APARECIDA DA SILVA DE OLIVEIRA
-        "8469504",  //DANIELE DE MELO OLIVEIRA DE CARVALHO
-        "8500860",  //DAVI AUGUSTO DE ARAUJO MACEDO
-        "9229787",  //DEBORA SEGURA PAIXAO
-        "9233563",  //DIRCE MARIA ROSA DE OLIVEIRA
-        "7106921",  //DORVAL DA COSTA TORRES
-        "7989946",  //EDSON LIMA
-        "6865682",  //ELAINE CRISTINA CARDOSO SETRINI
-        "8471622",  //ELDIENE SOARES DE OLIVEIRA
-        "8830649",  //ELINARDO NOGUEIRA COSTA
-        "9233512",  //ELIONAI CRUZ CONCEICAO COELHO
-        "8815585",  //EMERICK RANNYERY CHICOLLI AGUIAR
-        "8230901",  //FABIANA ANDRADE DE LACERDA SILVA
-        "8257833",  //FABIO FERREIRA RAMOS
-        "7871643",  //FERNANDA DIAS DA SILVA
-        "9191364",  //GABRIELA DA SILVA FERREIRA DE LIMA
-        "9114696",  //HANNA TEIXEIRA DIAS
-        "8087539",  //HEROS RODRIGUES DE MORAIS
-        "8840989",  //IVONE SIQUEIRA LUIZ
-        "8428930",  //JANAINA STEFANIE ALVARES HUBNER
-        "9212451",  //JULIA AZEREDO DA SILVA
-        "8964327",  //JOELMA DOS SANTOS
-        "8183031",  //JOSE AUGUSTO LOPES DE ALENCAR
-        "8970661",  //KAUANE SILVA SEVERINO
-        "8031304",  //LAERCIO JOSE DE DEUS JUNIOR
-        "8514631",  //LIGIA PERPETUO CAPELO FURUGUEM
-        "9156011",  //LILIANE REGINA LIMA SILVA
-        "6789587",  //LUIZ CARLOS CRISTAN
-        "8497036",  //MARIA APARECIDA PACCA DA SILVA
-        "7487959",  //MARIA DE FATIMA LIMA VIEIRA              
-        "7514981",  //NUBIA VIEIRA DE ARAUJO CERQUEIRA
-        "8839131",  //OLINDA ROBERTO LEANDRO
-        "7176783",  //ORLANDO NUNES DE BRITO
-        "7590547",  //PAULINA APARECIDA ZEFERINO
-        "8482985",  //PAULO SERGIO BRAZ
-        "8470634",  //RAFAEL BELMONTE SILVA
-        "9239863",  //RAFAEL DE MELO FERRAZ
-        "8427071",  //REJANE BORBA SANTIAGO SILVA
-        "8964858",  //RENATA DE ANDRADE ALVES
-        "8456542",  //RONALDO DE OLIVEIRA
-        "8456054",  //ROSANE ALEXANDRE ALVES
-        "7976470",  //ROSANGELA APARECIDA FONSECA
-        "8251592",  //SAMUEL FERNANDES CALDAS
-        "8346178",  //SAMUEL FERREIRA DE CAMPOS
-        "8163111",  //SANDRO RAMOS DOS SANTOS
-        "9145770",  //SARA GOIS NASCIMENTO
-        "8482616", // SUE ELLEN CRISTINA LUZ DE MELO CADETE
-        "8795568", // TIANE MARISE RODRIGUES CHAGAS
-        "8821917",  // TAUANA LOPES NEPONUCENO
-        "7546823",  //VERA LUCIA AYKO TAKARA
-        "8044368",  //VERONICE TAVEIRA DE MELO
-        "7919905",]  // VIVIANE CARVALHO DE OLIVEIRA
-    // ADICIONE OS RF NO FINAL DA LISTA ACIMA
-    // Lista de códigos a serem pesquisados
+    //const codesList = [
+
+    const codesListWithNames = [
+        // Lista de RF a serem pesquisados
+        { code: "9233521", name: "ADRIANO ANTUNES DE SALVES" },
+        { code: "8285187", name: "ALESSANDRA RAYMUNDO" },
+        { code: "7971427", name: "ALESSANDRA SERICAWA BROCCO SILVA" },
+        { code: "8398542", name: "AMANDA ALVES DOS SANTOS PEDRO" },
+        { code: "8029482", name: "ANA LUCIA CARVALHO" },
+        { code: "8791210", name: "ANA PAULA RODRIGUES HONORIO" },
+        { code: "8063494", name: "ANA PAULA VIANA COSTA" },
+        { code: "8821186", name: "ANGELA MARIA DE OLIVEIRA" },
+        { code: "8259640", name: "APARECIDA VIEIRA DA SILVA" },
+        { code: "8171831", name: "BEATRIZ CAROLINE BISPO DIAS" },
+        { code: "8421790", name: "BEATRIZ NOGUEIRA DE SOUSA" },
+        { code: "7822529", name: "CARLA FERNANDA BARRETO" },
+        { code: "8495955", name: "CAROLINA LIMA ANTUNES" },
+        { code: "8279071", name: "CELIA REGINA LUNA TOPGIAN" },
+        { code: "8163481", name: "CLAUDIA MARIA AMADOR RUSSO" },
+        { code: "8259011", name: "CLAUDINEI DA SILVA CRUZ" },
+        { code: "7736835", name: "CLAUDIO VIEIRA DA SILVA" },
+        { code: "8139539", name: "CRISTINA APARECIDA DA SILVA DE OLIVEIRA" },
+        { code: "8469504", name: "DANIELE DE MELO OLIVEIRA DE CARVALHO" },
+        { code: "8500860", name: "DAVI AUGUSTO DE ARAUJO MACEDO" },
+        { code: "9229787", name: "DEBORA SEGURA PAIXAO" },
+        { code: "9233563", name: "DIRCE MARIA ROSA DE OLIVEIRA" },
+        { code: "7106921", name: "DORVAL DA COSTA TORRES" },
+        { code: "7989946", name: "EDSON LIMA" },
+        { code: "6865682", name: "ELAINE CRISTINA CARDOSO SETRINI" },
+        { code: "8471622", name: "ELDIENE SOARES DE OLIVEIRA" },
+        { code: "8830649", name: "ELINARDO NOGUEIRA COSTA" },
+        { code: "9233512", name: "ELIONAI CRUZ CONCEICAO COELHO" },
+        { code: "8815585", name: "EMERICK RANNYERY CHICOLLI AGUIAR" },
+        { code: "8230901", name: "FABIANA ANDRADE DE LACERDA SILVA" },
+        { code: "8257833", name: "FABIO FERREIRA RAMOS" },
+        { code: "7871643", name: "FERNANDA DIAS DA SILVA" },
+        { code: "9191364", name: "GABRIELA DA SILVA FERREIRA DE LIMA" },
+        { code: "9114696", name: "HANNA TEIXEIRA DIAS" },
+        { code: "8087539", name: "HEROS RODRIGUES DE MORAIS" },
+        { code: "8840989", name: "IVONE SIQUEIRA LUIZ" },
+        { code: "8428930", name: "JANAINA STEFANIE ALVARES HUBNER" },
+        { code: "9212451", name: "JULIA AZEREDO DA SILVA" },
+        { code: "8964327", name: "JOELMA DOS SANTOS" },
+        { code: "8183031", name: "JOSE AUGUSTO LOPES DE ALENCAR" },
+        { code: "8970661", name: "KAUANE SILVA SEVERINO" },
+        { code: "8031304", name: "LAERCIO JOSE DE DEUS JUNIOR" },
+        { code: "8514631", name: "LIGIA PERPETUO CAPELO FURUGUEM" },
+        { code: "9156011", name: "LILIANE REGINA LIMA SILVA" },
+        { code: "6789587", name: "LUIZ CARLOS CRISTAN" },
+        { code: "8497036", name: "MARIA APARECIDA PACCA DA SILVA" },
+        { code: "7487959", name: "MARIA DE FATIMA LIMA VIEIRA" },
+        { code: "7514981", name: "NUBIA VIEIRA DE ARAUJO CERQUEIRA" },
+        { code: "8839131", name: "OLINDA ROBERTO LEANDRO" },
+        { code: "7176783", name: "ORLANDO NUNES DE BRITO" },
+        { code: "7590547", name: "PAULINA APARECIDA ZEFERINO" },
+        { code: "8482985", name: "PAULO SERGIO BRAZ" },
+        { code: "8470634", name: "RAFAEL BELMONTE SILVA" },
+        { code: "9239863", name: "RAFAEL DE MELO FERRAZ" },
+        { code: "8427071", name: "REJANE BORBA SANTIAGO SILVA" },
+        { code: "8964858", name: "RENATA DE ANDRADE ALVES" },
+        { code: "8456542", name: "RONALDO DE OLIVEIRA" },
+        { code: "8456054", name: "ROSANE ALEXANDRE ALVES" },
+        { code: "7976470", name: "ROSANGELA APARECIDA FONSECA" },
+        { code: "8251592", name: "SAMUEL FERNANDES CALDAS" },
+        { code: "8346178", name: "SAMUEL FERREIRA DE CAMPOS" },
+        { code: "8163111", name: "SANDRO RAMOS DOS SANTOS" },
+        { code: "9145770", name: "SARA GOIS NASCIMENTO" },
+        { code: "8482616", name: "SUE ELLEN CRISTINA LUZ DE MELO CADETE" },
+        { code: "8795568", name: "TIANE MARISE RODRIGUES CHAGAS" },
+        { code: "8821917", name: "TAUANA LOPES NEPONUCENO" },
+        { code: "7546823", name: "VERA LUCIA AYKO TAKARA" },
+        { code: "8044368", name: "VERONICE TAVEIRA DE MELO" },
+        { code: "7919905", name: "VIVIANE CARVALHO DE OLIVEIRA" }
+        // Adicione mais códigos e nomes se necessário
+    ];
 
     if (selectedFile) {
         const fileReader = new FileReader();
         fileReader.onload = function (e) {
             const pdfData = new Uint8Array(e.target.result);
 
-            // Use a biblioteca pdf.js para carregar o PDF
             pdfjsLib.getDocument(pdfData).promise.then(function (pdfDoc) {
                 const textPromises = [];
 
@@ -118,49 +119,48 @@ function searchCodes() {
                 }
 
                 Promise.all(textPromises).then(function (pages) {
-                    // Inicializa um objeto que mapeia os códigos às linhas
                     const codeToLinesMap = {};
-                    codesList.forEach(code => {
-                        codeToLinesMap[code] = [];
+                    codesListWithNames.forEach(item => {
+                        codeToLinesMap[item.code] = { name: item.name, lines: [] };
                     });
 
-                    // Percorre as páginas e busca os códigos
                     for (let i = 0; i < pages.length; i++) {
                         const pageText = pages[i].items.map(item => item.str).join('\n');
                         const lines = pageText.split('\n');
 
-                        for (const code of codesList) {
+                        for (const item of codesListWithNames) {
+                            const code = item.code;
                             for (const line of lines) {
                                 if (line.includes(code)) {
-                                    codeToLinesMap[code].push(line);
+                                    codeToLinesMap[code].lines.push(line);
                                 }
                             }
                         }
                     }
 
-                    // Exibe os resultados
-                    if (codesList.length === 0) {
+                    if (codesListWithNames.length === 0) {
                         resultsDiv.innerHTML = "Nenhum RF encontrado nesta Edição";
                     } else {
                         resultsDiv.innerHTML = "Busca Concluída... <br><br>" +
-                            "RFs encontradas no Diário Oficial desta Edição <br><br>";
+                            "Servidores encontrados no Diário Oficial desta Edição <br><br>";
 
-                        let foundRF = false; // Variável para verificar se pelo menos um RF foi encontrado
+                        let foundRF = false;
 
-                        for (const code of codesList) {
-                            const lines = codeToLinesMap[code];
+                        for (const item of codesListWithNames) {
+                            const code = item.code;
+                            const lines = codeToLinesMap[code].lines;
                             if (lines.length > 0) {
-                                resultsDiv.innerHTML += `RF :     -  ${code} <br>`;
-                                foundRF = true; // Um RF foi encontrado
+                                const name = codeToLinesMap[code].name;
+                                resultsDiv.innerHTML += ` - RF:  ${code} - Nome: ${name} <br>`;
+                                foundRF = true;
                             }
                         }
 
                         if (!foundRF) {
-                            resultsDiv.innerHTML = " * Nenhum RF encontrado nesta Edição<br>"; // Exibe a mensagem se nenhum RF for encontrado
+                            resultsDiv.innerHTML = " * Nenhum Servidor encontrado nesta Edição<br>";
                         }
                     }
 
-                    // Chame a função para ocultar a barra de aguarde após a busca estar completa
                     ocultarBarraDeAguarde();
 
                 });
@@ -169,7 +169,6 @@ function searchCodes() {
         fileReader.readAsArrayBuffer(selectedFile);
     } else {
         resultsDiv.innerHTML = "Atenção: Selecione o arquivo PDF da Edição." + "<br>";
-        // Chame a função para ocultar a barra de aguarde em caso de erro
         ocultarBarraDeAguarde();
     }
 }
