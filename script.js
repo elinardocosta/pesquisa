f6dac3d019d8445e6726b1574443be903ee2cf52
Elinardo <costa.elinardo@gmail.com>
2024-02-19 14:11:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,88 @@
+/* Adicione os scripts para abrir e fechar o popup -->
+ // Lista de RF a serem pesquisados
+        { code: "8285187", name: "ALESSANDRA RAYMUNDO" }, { code: "828.518.7", name: "ALESSANDRA RAYMUNDO" },
+        { code: "7971427", name: "ALESSANDRA SERICAWA BROCCO SILVA" }, { code: "797.142.7", name: "ALESSANDRA SERICAWA BROCCO SILVA" },
+        { code: "8398542", name: "AMANDA ALVES DOS SANTOS PEDRO" }, { code: "839.854.2", name: "AMANDA ALVES DOS SANTOS PEDRO" },
+        { code: "8029482", name: "ANA LUCIA CARVALHO" }, { code: "802.948.2", name: "ANA LUCIA CARVALHO" },
+        { code: "8791210", name: "ANA PAULA RODRIGUES HONORIO" }, { code: "879.121.0", name: "ANA PAULA RODRIGUES HONORIO" },
+        { code: "8063494", name: "ANA PAULA VIANA COSTA" }, { code: "806.349.4", name: "ANA PAULA VIANA COSTA" },
+        { code: "8821186", name: "ANGELA MARIA DE OLIVEIRA" }, { code: "882.118.6", name: "ANGELA MARIA DE OLIVEIRA" },
+        { code: "8978620", name: "ANGELA SELMA DA SILVA E MELO" }, { code: "897.862.0", name: "ANGELA SELMA DA SILVA E MELO" },
+        { code: "8109788", name: "ANTONIO CARLOS SACRAMENTODA CONCEICAO" }, { code: "810.978.8", name: "ANTONIO CARLOS SACRAMENTODA CONCEICAO" },
+        { code: "8259640", name: "APARECIDA VIEIRA DA SILVA" }, { code: "825.964.0", name: "APARECIDA VIEIRA DA SILVA" },
+        { code: "9153055", name: "BEATRIZ BRANDAO" }, { code: "915.305.5", name: "BEATRIZ BRANDAO" },
+        { code: "8171831", name: "BEATRIZ CAROLINE BISPO DIAS" }, { code: "817.183.1", name: "BEATRIZ CAROLINE BISPO DIAS" },
+        { code: "8421790", name: "BEATRIZ NOGUEIRA DE SOUSA" }, { code: "842.179.0", name: "BEATRIZ NOGUEIRA DE SOUSA" },
+        { code: "7449607", name: "BENEDITA LIMA KECEK" }, { code: "744.960.7", name: "BENEDITA LIMA KECEK" },
+        { code: "7916744", name: "BRUNO ALEXANDRE DEANINI" }, { code: "791.674.4", name: "BRUNO ALEXANDRE DEANINI" },
+        { code: "9145761", name: "BRUNO CAUE DE SOUZA" }, { code: "914.576.1", name: "BRUNO CAUE DE SOUZA" },
+        { code: "7822529", name: "CARLA FERNANDA BARRETO" }, { code: "782.252.9", name: "CARLA FERNANDA BARRETO" },
+        { code: "8965021", name: "CARLOS ROBERTO LANDIM SOUZA SANTOS" }, { code: "896.502.1", name: "CARLOS ROBERTO LANDIM SOUZA SANTOS" },
+        { code: "8495955", name: "CAROLINA LIMA ANTUNES" }, { code: "849.595.5", name: "CAROLINA LIMA ANTUNES" },
+        { code: "8279071", name: "CELIA REGINA LUNA TOPGIAN" }, { code: "827.907.1", name: "CELIA REGINA LUNA TOPGIAN" },
+        { code: "8023646", name: "CILENE APARECIDA PALMA SOARES GUIERO" }, { code: "802.364.6", name: "CILENE APARECIDA PALMA SOARES GUIERO" },
+        { code: "8163481", name: "CLAUDIA MARIA AMADOR RUSSO" }, { code: "816.348.1", name: "CLAUDIA MARIA AMADOR RUSSO" },
+        { code: "7736835", name: "CLAUDIO VIEIRA DA SILVA" }, { code: "773.683.5", name: "CLAUDIO VIEIRA DA SILVA" },
+        { code: "8816565", name: "DAIANA CRISTINA CUBAS" }, { code: "881.656.5", name: "DAIANA CRISTINA CUBAS" },
+        { code: "8207704", name: "DANIELA TAVARES MAXIMO DA SILVA" }, { code: "820.770.4", name: "DANIELA TAVARES MAXIMO DA SILVA" },
+        { code: "8469504", name: "DANIELE DE MELO OLIVEIRA DE CARVALHO" }, { code: "846.950.4", name: "DANIELE DE MELO OLIVEIRA DE CARVALHO" },
+        { code: "8500860", name: "DAVI AUGUSTO DE ARAUJO MACEDO" }, { code: "850.086.0", name: "DAVI AUGUSTO DE ARAUJO MACEDO" },
+        { code: "8039950", name: "DAYANA CUNHA ZANETTI" }, { code: "803.995.0", name: "DAYANA CUNHA ZANETTI" },
+        { code: "9233563", name: "DIRCE MARIA ROSA DE OLIVEIRA" }, { code: "923.356.3", name: "DIRCE MARIA ROSA DE OLIVEIRA" },
+        { code: "6865682", name: "ELAINE CRISTINA CARDOSO SETRINI" }, { code: "686.568.2", name: "ELAINE CRISTINA CARDOSO SETRINI" },
+        { code: "8471622", name: "ELDIENE SOARES DE OLIVEIRA" }, { code: "847.162.2", name: "ELDIENE SOARES DE OLIVEIRA" },
+        { code: "8830649", name: "ELINARDO NOGUEIRA COSTA" }, { code: "883.064.9", name: "ELINARDO NOGUEIRA COSTA" },
+        { code: "9233512", name: "ELIONAI CRUZ CONCEICAO COELHO" }, { code: "923.351.2", name: "ELIONAI CRUZ CONCEICAO COELHO" },
+        { code: "7954123", name: "ELOIZA DA SILVA SOUZA" }, { code: "795.412.3", name: "ELOIZA DA SILVA SOUZA" },
+        { code: "8230901", name: "FABIANA ANDRADE DE LACERDA SILVA" }, { code: "823.090.1", name: "FABIANA ANDRADE DE LACERDA SILVA" },
+        { code: "8936803", name: "ESTEVAO MARIO LINO" }, { code: "893.680.3", name: "ESTEVAO MARIO LINO" },
+        { code: "7270984", name: "FABIANE DOS SANTOS" }, { code: "727.098.4", name: "FABIANE DOS SANTOS" },
+        { code: "8257833", name: "FABIO FERREIRA RAMOS" }, { code: "825.783.3", name: "FABIO FERREIRA RAMOS" },
+        { code: "9241825", name: "FERNANDA DE CASSIA GALVAO BASTOS" }, { code: "924.182.5", name: "FERNANDA DE CASSIA GALVAO BASTOS" },
+        { code: "7871643", name: "FERNANDA DIAS DA SILVA" }, { code: "787.164.3", name: "FERNANDA DIAS DA SILVA" },
+        { code: "8087539", name: "HEROS RODRIGUES DE MORAIS" }, { code: "808.753.9", name: "HEROS RODRIGUES DE MORAIS" },
+        { code: "9229469", name: "IVANIA ALVES GOMES DA SILVA" }, { code: "922.946.9", name: "IVANIA ALVES GOMES DA SILVA" },
+        { code: "8428930", name: "JANAINA STEFANIE ALVARES HUBNER" }, { code: "842.893.0", name: "JANAINA STEFANIE ALVARES HUBNER" },
+        { code: "8923892", name: "JAQUELINE COSTA DOS AJOS AZAVEDO" }, { code: "892.389.2", name: "JAQUELINE COSTA DOS AJOS AZAVEDO" },
+        { code: "9212451", name: "JULIA AZEREDO DA SILVA" }, { code: "921.245.1", name: "JULIA AZEREDO DA SILVA" },
+        { code: "8183031", name: "JOSE AUGUSTO LOPES DE ALENCAR" }, { code: "818.303.1", name: "JOSE AUGUSTO LOPES DE ALENCAR" },
+        { code: "9287507", name: "KAWE VINICIUS GONCALVES DE SOUZA" }, { code: "928.750.7", name: "KAWE VINICIUS GONCALVES DE SOUZA" },
+        { code: "9232923", name: "KELLY CRISTINA ALVES FEITOSA" }, { code: "923.292.3", name: "KELLY CRISTINA ALVES FEITOSA" },
+        { code: "8031304", name: "LAERCIO JOSE DE DEUS JUNIOR" }, { code: "803.130.4", name: "LAERCIO JOSE DE DEUS JUNIOR" },
+        { code: "9156011", name: "LILIANE REGINA LIMA SILVA" }, { code: "915.601.1", name: "LILIANE REGINA LIMA SILVA" },
+        { code: "6789587", name: "LUIZ CARLOS CRISTAN" }, { code: "678.958.7", name: "LUIZ CARLOS CRISTAN" },
+        { code: "8844909", name: "MARCO ANTONIO DE ALMEIDA" }, { code: "884.490.9", name: "MARCO ANTONIO DE ALMEIDA" },
+        { code: "7969520", name: "MARIA APARECIDA FERNANDES DOS SANTOS SEVERINO" }, { code: "796.952.0", name: "MARIA APARECIDA FERNANDES DOS SANTOS SEVERINO" },
+        { code: "8497036", name: "MARIA APARECIDA PACCA DA SILVA" }, { code: "849.703.6", name: "MARIA APARECIDA PACCA DA SILVA" },
+        { code: "6885349", name: "MARIA JOSE DA SILVA" }, { code: "688.534.9", name: "MARIA JOSE DA SILVA" },
+        { code: "7871848", name: "MARISA GOI DE OLIVEIRA" }, { code: "787.184.8", name: "MARISA GOI DE OLIVEIRA" },
+        { code: "9243232", name: "MICHELLE CARLA VICENTE MARQUES" }, { code: "924.323.2", name: "MICHELLE CARLA VICENTE MARQUES" },
+        { code: "7926898", name: "MILTON UCHOA SA JUNIOR" }, { code: "792.689.8", name: "MILTON UCHOA SA JUNIOR" },
+        { code: "8839131", name: "OLINDA ROBERTO LEANDRO" }, { code: "883.913.1", name: "OLINDA ROBERTO LEANDRO" },
+        { code: "8083452", name: "PATRICIA APARECIDA DA SILVA GRANDE" }, { code: "808.345.2", name: "PATRICIA APARECIDA DA SILVA GRANDE" },
+        { code: "8482985", name: "PAULO SERGIO BRAZ" }, { code: "848.298.5", name: "PAULO SERGIO BRAZ" },
+        { code: "8470634", name: "RAFAEL BELMONTE SILVA" }, { code: "847.063.4", name: "RAFAEL BELMONTE SILVA" },
+        { code: "8427071", name: "REJANE BORBA SANTIAGO SILVA" }, { code: "842.707.1", name: "REJANE BORBA SANTIAGO SILVA" },
+        { code: "7877447", name: "RODRIGO ALVES QUINTIERE" }, { code: "787.744.7", name: "RODRIGO ALVES QUINTIERE" },
+        { code: "8456542", name: "RONALDO DE OLIVEIRA" }, { code: "845.654.2", name: "RONALDO DE OLIVEIRA" },
+        { code: "8456054", name: "ROSANE ALEXANDRE ALVES" }, { code: "845.605.4", name: "ROSANE ALEXANDRE ALVES" },
+        { code: "7976470", name: "ROSANGELA APARECIDA FONSECA" }, { code: "797.647.0", name: "ROSANGELA APARECIDA FONSECA" },
+        { code: "7866534", name: "ROSEMEIRE JUSTINO DOS SANTOS DE OLIVEIRA" }, { code: "786.653.4", name: "ROSEMEIRE JUSTINO DOS SANTOS DE OLIVEIRA" },
+        { code: "9243771", name: "ROSILEIDE BELCHIOR DE ARARUNA" }, { code: "924.377.1", name: "ROSILEIDE BELCHIOR DE ARARUNA" },
+        { code: "8346178", name: "SAMUEL FERREIRA DE CAMPOS" }, { code: "834.617.8", name: "SAMUEL FERREIRA DE CAMPOS" },
+        { code: "8163111", name: "SANDRO RAMOS DOS SANTOS" }, { code: "816.311.1", name: "SANDRO RAMOS DOS SANTOS" },
+        { code: "8482616", name: "SUE ELLEN CRISTINA LUZ DE MELO CADETE" }, { code: "848.261.6", name: "SUE ELLEN CRISTINA LUZ DE MELO CADETE" },
+        { code: "8795568", name: "TATIANE MARISE RODRIGUES CHAGAS" }, { code: "879.556.8", name: "TATIANE MARISE RODRIGUES CHAGAS" },
+        { code: "8821917", name: "TAUANA LOPES NEPONUCENO" }, { code: "882.191.7", name: "TAUANA LOPES NEPONUCENO" },
+        { code: "7546823", name: "VERA LUCIA AYKO TAKARA" }, { code: "754.682.3", name: "VERA LUCIA AYKO TAKARA" },
+        { code: "8044368", name: "VERONICE TAVEIRA DE MELO" }, { code: "804.436.8", name: "VERONICE TAVEIRA DE MELO" },
+        { code: "7919905", name: "VIVIANE CARVALHO DE OLIVEIRA" }, { code: "791.990.5", name: "VIVIANE CARVALHO DE OLIVEIRA" }
+        // Adicione mais códigos e nomes se necessário
+*/
+
 /*scripts para abrir e fechar o popup --> */
+
+
 function openPopup() {
     document.getElementById("overlay").style.display = "flex";
 }
@@ -30,7 +114,6 @@ document.addEventListener("DOMContentLoaded", function () {
     function closePopup() {
         $("#overlay").fadeOut();
     }
-
 
     // Adicione um ouvinte de evento para o formulário
     codeForm.addEventListener("submit", function (e) {
@@ -80,7 +163,6 @@ function ocultarBarraDeAguarde() {
     var loading = document.getElementById("loading");
     loading.style.display = "none";
 }
-/* fim do scripts para abrir e fechar o popup --> */
 
 function searchCodes() {
     ocultarLabel();
@@ -94,6 +176,7 @@ function searchCodes() {
 
     const selectedFile = fileInput.files[0];
     const codesListWithNames = [
+        // Lista de RF a serem pesquisados
         // Lista de RF a serem pesquisados
         { code: "8285187", name: "ALESSANDRA RAYMUNDO" }, { code: "828.518.7", name: "ALESSANDRA RAYMUNDO" },
         { code: "7971427", name: "ALESSANDRA SERICAWA BROCCO SILVA" }, { code: "797.142.7", name: "ALESSANDRA SERICAWA BROCCO SILVA" },
@@ -173,13 +256,20 @@ function searchCodes() {
         { code: "8044368", name: "VERONICE TAVEIRA DE MELO" }, { code: "804.436.8", name: "VERONICE TAVEIRA DE MELO" },
         { code: "7919905", name: "VIVIANE CARVALHO DE OLIVEIRA" }, { code: "791.990.5", name: "VIVIANE CARVALHO DE OLIVEIRA" }
         // Adicione mais códigos e nomes se necessário
+        // Códigos e nomes omitidos para brevidade
     ];
 
     if (selectedFile) {
+        // Verifica se o arquivo selecionado é um PDF
+        if (selectedFile.type !== 'application/pdf') {
+            resultsDiv.innerHTML = "<br><span style='font-size: 24px;'>Atenção: Selecione um arquivo .PDF </span>" + "<br>";
+            ocultarBarraDeAguarde();
+            return; // Sai da função se não for um PDF
+        }
+
         const fileReader = new FileReader();
         fileReader.onload = function (e) {
             const pdfData = new Uint8Array(e.target.result);
-
             pdfjsLib.getDocument(pdfData).promise.then(function (pdfDoc) {
                 const textPromises = [];
 
@@ -217,12 +307,13 @@ function searchCodes() {
                     }
 
                     // Exibe os resultados
+
                     if (results.length > 0) {
                         let tableHTML = `
                             <table>
                                 <tr>
                                     <th>RF</th>
-                                    <th>Nome</th>
+                                    <th>Nome do Servidor</th>
                                     <th>Página do D.O</th>
                                 </tr>`;
 
@@ -236,9 +327,10 @@ function searchCodes() {
                         }
 
                         tableHTML += "</table>";
+
                         resultsDiv.innerHTML = tableHTML;
                     } else {
-                        resultsDiv.innerHTML = "<br><span style='font-size: 20px;'>Nenhum resultado encontrado.</span><br>";
+                        resultsDiv.innerHTML = "<br><span style='font-size: 24px;'>Nenhum resultado encontrado.</span><br>";
                     }
 
                     ocultarBarraDeAguarde();
@@ -247,23 +339,7 @@ function searchCodes() {
         };
         fileReader.readAsArrayBuffer(selectedFile);
     } else {
-        resultsDiv.innerHTML = "<br><span style='font-size: 20px;'>Atenção: Selecione o arquivo PDF da Edição.</span>" + "<br>";
+        resultsDiv.innerHTML = "<br><span style='font-size: 24px;'>Atenção: Selecione o arquivo PDF da Edição.</span>" + "<br>";
         ocultarBarraDeAguarde();
     }
-}
-
-function ocultarLabel() {
-    var label = document.getElementById("minhaLabel");
-    label.style.display = "none";
-}
-
-function mostrarBarraDeAguarde() {
-    var loading = document.getElementById("loading");
-    loading.style.display = "block";
-}
-
-function ocultarBarraDeAguarde() {
-    document.getElementById("minhaLabel").style.display = "none";
-    var loading = document.getElementById("loading");
-    loading.style.display = "none";
 }
